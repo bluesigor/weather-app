ec48565302ed2bf9d3b152ebd89f88b0e149423e
bluesigor <austrianlwoos@gmail.com>
2023-08-28 16:39:27 +0200

    feat/images

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import "./styles.css";
+import react_logo from "./react.png";
+import boat from "./boat.svg";
+
 export const App = () => {
-  return <h1>Weather app build on top of custom Webpack</h1>;
+  return (
+    <>
+      <img src={react_logo} alt="react" />
+      <h1>Weather app build on top of custom Webpack</h1>
+      <img src={boat} alt="boat" width={300} />
+    </>
+  );
 };
