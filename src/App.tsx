a8d43f40fd14cffa5bd29952dc1f5bdad81be9d6
bluesigor <austrianlwoos@gmail.com>
2023-08-29 13:49:49 +0200

    husky changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import "./styles.css";
-import react_logo from "./react.png";
-import boat from "./boat.svg";
-import ClickCounter from "./ClickCounter";
+import './styles.css'
+import react_logo from './react.png'
+import boat from './boat.svg'
+import ClickCounter from './ClickCounter'
 
 export const App = () => {
-  const name = "Ihor";
+  const name = 'Ihor'
 
   return (
     <>
       <img src={react_logo} alt="react" />
       <img src={boat} alt="boat" width={300} />
       <h1>
-        Weather app build on top of custom Webpack - {process.env.NODE_ENV} -{" "}
+        Weather app build on top of custom Webpack - {process.env.NODE_ENV} -{' '}
         {process.env.name}
       </h1>
       <ClickCounter />
     </>
-  );
-};
+  )
+}
--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const ClickCounter = () => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0)
 
   const incrementByOne = () => {
-    setCounter((prevCounter) => prevCounter + 1);
-    setCounter((prevCounter) => prevCounter + 1);
-  };
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+  }
 
   return (
     <div>
       <button onClick={incrementByOne}>Count {counter}</button>
     </div>
-  );
-};
+  )
+}
 
-export default ClickCounter;
+export default ClickCounter
